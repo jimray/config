a0971c6ad9a24788acac747f36b38be2afee73d6
Jim Ray <jray@slack-corp.com>
2017-07-15 16:52:55 -0700

    Started a bootstrap file

---
+++ .bootstrap.sh
@@ -0,0 +1,16 @@
+# This doesn't do much yet, mostly for record keeping
+# Set up vim for plugins
+mkdir -p ~/.vim/pack/plugins/start/
+mkdir -p ~/.vim/colors
+
+# Grab vim plugins - maybe move these into .vimrc and grep for them?
+git clone https://github.com/tpope/vim-surround.git ~/.vim/pack/plugins/start/vim-surround
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/plugins/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/vim-airline-themes
+
+# Make vim pretty
+curl 'https://raw.githubusercontent.com/mhartington/oceanic-next/master/colors/OceanicNext.vim' > ~/.vim/colors/oceanic-next.vim
+curl 'https://raw.githubusercontent.com/mhartington/oceanic-next/master/autoload/airline/themes/oceanicnext.vim' > ~/.vim/pack/plugins/start/vim-airline-themes/autoload/airline/themes
+# Oceanic for iTerm, too
+# https://raw.githubusercontent.com/mhartington/oceanic-next-iterm/master/Oceanic-Next.itermcolors
+# TODO figure out how to automate iTerm's colors
