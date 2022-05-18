7664d161decb14d6ad3ee06565dba5bbb5263c80
Jim Ray <jray@slack-corp.com>
2022-05-18 14:08:45 -0400

    added vim-commentary

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -25,11 +25,12 @@ mkdir -p ~/.vim/colors/
 
 # Grab vim plugins
 git clone https://github.com/tpope/vim-surround.git ~/.vim/pack/plugins/start/vim-surround
+git clone https://github.com/tpope/vim-vinegar.git ~/.vim/pack/plugins/start/vim-vinegar
+git clone https://github.com/tpope/vim-commentary.git ~/.vim/pack/plugins/start/vim-commentary
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/plugins/start/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/vim-airline-themes
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/pack/plugins/start/editorconfig-vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-git clone https://github.com/tpope/vim-vinegar.git ~/.vim/pack/plugins/start/vim-vinegar
 git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
 git clone https://github.com/plasticboy/vim-markdown.git ~/.vim/pack/plugins/start/vim-markdown
 git clone https://github.com/reedes/vim-pencil.git ~/.vim/pack/plugins/start/vim-pencil
@@ -38,6 +39,12 @@ git clone --depth 1 https://github.com/adrian5/oceanic-next-vim ~/.vim/pack/plug
 # fzf requires both the binary with the fzf plugin and an additional plugin :shrug:
 git clone http://github.com/junegunn/fzf ~/.vim/pack/plugins/start/fzf
 git clone http://github.com/junegunn/fzf.vim ~/.vim/pack/plugins/start/fzf.vim
+
+# vim help config
+vim -u NONE -c "helptags ~/.vim/pack/plugins/start/vim-airline/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/plugins/start/vim-pencil/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/plugins/start/vim-surround/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/plugins/start/vim-commentary/doc" -c q
 
 # Install z
 git clone https://github.com/rupa/z.git ~/._z
